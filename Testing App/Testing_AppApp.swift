774d950630ba49a986fe89950f65eae94884d293
Daniel Wylie <daniel@danielwylie.me>
2025-05-12 14:36:21 +1200

    And make the app run

--- Testing App/Testing_AppApp.swift
+++ Testing App/Testing_AppApp.swift
@@ -5,10 +5,15 @@
 //  Created by Daniel Wylie on 09/05/2025.
 //
 
+import PanelPop
 import SwiftUI
 
 @main
 struct Testing_AppApp: App {
+    init() {
+        PanelPop.initialize("pp1_c60b3357323d5c882525507c5015733a02c5a9d529f10bea3c10627356f9")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
